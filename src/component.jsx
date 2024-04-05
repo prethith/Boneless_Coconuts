366392515460daf64973ef1844847ddd079cc163
Amulya Vince Chemparathy <142325507+AmulyaVinceChemparathy@users.noreply.github.com>
2024-04-05 19:01:23 +0530

    added time

--- src/component.jsx
+++ src/component.jsx
@@ -9,14 +9,15 @@ const PopupComponent = ({}) => {
     
         <div className="popup">
           <div className="popup-content">
-            <span className="close" >&times;</span>
-            <h2>Enter Name and Date</h2>
-            <form >
+            <h2 class="h2">Enter Name and Date</h2><br></br>
+            <form class="form">
           
               <label htmlFor="name" class="form">Name:</label>
               <input type="text" id="name"  /><br></br>
               <label htmlFor="date"  class="form">Date:</label>
               <input type="date" id="date"  /><br></br>
+              <label htmlFor="time"  class="form">Time:</label>
+              <input type="time" id="time"  /><br></br>
               <button type="submit" id="submit">Submit</button>
             </form>
           </div>
